d394253464c8b0f3b4149cf907d8348bf81b1457
JamesBond <introbond.dev@gmail.com>
2023-05-01 18:27:26 +0700

    Update: install curl

--- setup-zsh/main.sh
+++ setup-zsh/main.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+install_curl() {
+    sudo apt-get update
+    sudo apt-get install curl
+}
+
 add_zsh() {
     sudo apt-get install zsh
 }
@@ -13,6 +18,7 @@ add_plugin () {
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
+install_curl
 add_zsh
 add_omzsh
 add_plugin
