ff32b3ee6ac51dc7c1d49cb3152694ed48122a38
JamesBond <introbond.dev@gmail.com>
2023-05-07 13:50:12 +0700

    Update: chrome

--- setup-chrome/main.sh
+++ setup-chrome/main.sh
@@ -5,10 +5,9 @@ R="$(printf '\033[1;31m')"
 G="$(printf '\033[1;32m')"
 Y="$(printf '\033[1;33m')"
 W="$(printf '\033[1;37m')"
-# C="$(printf '\033[1;36m')"
+C="$(printf '\033[1;36m')"
 
 banner(){
-clear
 printf ${R}"████████╗██████╗ \n"
 printf     "╚══██╔══╝██╔══██╗\n"
 printf     "   ██║   ██████╔╝\n"
@@ -19,14 +18,15 @@ printf ${Y}"                By James-Bond\n"${W}
 }
 
 function install_package {
+    echo ${G}"Installing $1 ..."${W}
     command -v $1 &> /dev/null && {
         echo
-        echo "$1 already installed."
+        echo ${C}"$1 already installed."${W}
         echo
         return 0
     }
     echo
-    echo "$1 not found. Installing $1..."
+    echo ${G}"$1 not found. Installing $1..."${W}
     echo
     sudo apt-get update
     sudo apt-get install $1 -y
