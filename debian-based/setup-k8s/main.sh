e1017787145740eead4dd16b813c0116bc8636d8
JamesBond <introbond.dev@gmail.com>
2023-06-05 14:25:11 +0700

    Update: setup minikube

--- debian-based/setup-k8s/main.sh
+++ debian-based/setup-k8s/main.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+minikube () {
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+}
+
 install () {
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
@@ -7,4 +12,9 @@ install () {
     kubectl version --client
 }
 
+minikube
 install
+
+# to do
+# minikube status
+# minikube start
