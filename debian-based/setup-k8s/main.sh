629ae3935557adad02dc30b18e37715ad268b4f8
JamesBond <introbond.dev@gmail.com>
2023-06-05 13:58:33 +0700

    Add: setup k8s

---
+++ debian-based/setup-k8s/main.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+install () {
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    kubectl version --client
+}
+
+install
