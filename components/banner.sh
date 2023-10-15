115a80a7344b5ab8494e4ab85d5042bf222ead47
JamesBond <introbond.dev@gmail.com>
2023-10-15 15:30:30 +0700

    Refactor

--- components/banner.sh
+++ components/banner.sh
@@ -6,14 +6,18 @@ USER_NAME=$(whoami)
 HOST_NAME=$(hostname)
 OS_NAME=$(lsb_release -ds)
 TIMEZONE=$(timedatectl show --property=Timezone --value)
-FOLLOW_ME="${GREEN}ABOUT ME: ${WHITE}https://thejb.onrender.com${RESET}"
+FOLLOW_ME="${WHITE}https://thejb.onrender.com${RESET}"
+
+
+banner="\n
+$(use_style $WHITE ====================================) \n
+USER: $(use_style $WHITE $USER_NAME) | MACHINE NAME: $(use_style $WHITE $HOST_NAME) \n
+OS: $(use_style $WHITE "$OS_NAME") \n
+TIME ZONE: $(use_style $WHITE "$TIMEZONE") \n
+OS: $(use_style $WHITE "$OS_NAME") \n
+ABOUT ME: $(use_style "$FOLLOW_ME") \n
+$(use_style $WHITE ====================================) \n"
 
 render_banner() {
-    echo "==============================================="
-    echo "USER: $(use_style $WHITE $USER_NAME) | MACHINE NAME: $(use_style $WHITE $HOST_NAME)"
-    echo "  OS: $(use_style $WHITE "$OS_NAME")"
-    echo "  TIME ZONE: $(use_style $WHITE "$TIMEZONE")"
-    echo "  $(use_style "$FOLLOW_ME")"
-    echo "==============================================="
-    echo ""
+    echo -e $banner
 }
