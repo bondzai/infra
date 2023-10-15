86c7692eacf424373e3b075475aff2d31daa62a2
JamesBond <introbond.dev@gmail.com>
2023-10-15 11:32:43 +0700

    Refactor

--- distro-debian/setup-version-manager/pyenv/install_pyenv_prereq.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
