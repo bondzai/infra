0008e9e8b81392946ebb29aed480df5f7e4ea12b
JamesBond <introbond.dev@gmail.com>
2023-10-15 10:25:41 +0700

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+installer=""
+
+# Read and install packages from deps.list
+while IFS= read -r line; do
+    if [[ $line == \#* ]]; then
+        continue
+    fi
+
+    if [[ $line == apt:* ]]; then
+        echo "Updating apt package manager..."
+        sudo apt update
+        installer="sudo apt install -y"
+
+    elif [[ $line == snap:* ]]; then
+        installer="sudo snap install"
+
+    elif [[ -n $installer ]]; then
+        package_name=${line#- }
+        echo "Installing $package_name..."
+        $installer "$package_name"
+    fi
+done < deps.list
+
+echo "All dependencies installed!"
