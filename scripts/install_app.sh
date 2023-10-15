d0804f9cf9a517fe902e0f9901ce104dc3555471
JamesBond <introbond.dev@gmail.com>
2023-10-15 19:41:19 +0700

    Refactor

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 echo " ===== start install app ===== "
+
+packages=("curl" "wget")
+
+for package in "${packages[@]}"; do
+    setup_deps "$package"
+done
--- utils/utils.sh
+++ utils/utils.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Define a function to install a package using apt-get
-function install_package {
+function setup_deps {
+    echo
+    echo -e ${GREEN}"Installing $1 ..."${WHITE}
     command -v $1 &> /dev/null && {
-        echo "$1 already installed."
+        echo
+        echo -e ${CYAN}"$1 already installed."${WHITE}
+        echo
         return 0
     }
-    echo "$1 not found. Installing $1..."
+    echo
+    echo -e ${GREEN}"$1 not found. Installing $1..."${WHITE}
+    echo
     sudo apt-get update
     sudo apt-get install $1 -y
 }
-
