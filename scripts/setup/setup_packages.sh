0a1ac8db9e222852d69b5f5b9a559ec6269872cb
JamesBond <introbond.dev@gmail.com>
2023-10-20 13:57:50 +0700

    Add k8s

--- scripts/setup/setup_packages.sh
+++ scripts/setup/setup_packages.sh
@@ -4,7 +4,12 @@ setup_docker() {
 }
 
 setup_k8s() {
-    echo -e "setup k8s..."
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    kubectl version --client
 }
 
 setup_wireguard() {
