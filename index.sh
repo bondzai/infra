43b6616569b8a93d4d260fa6611c8b0b256045f5
JamesBond <introbond.dev@gmail.com>
2023-10-20 17:37:11 +0700

    Update dynamic menu

--- index.sh
+++ index.sh
@@ -4,43 +4,71 @@ source "$(dirname $0)/utils/utils.sh"
 source "$(dirname $0)/components/banner.sh"
 source "$(dirname $0)/components/styles.sh"
 
-CONFIG_FILE="$(dirname $0)/config/index.conf"
+INDEX_LIST="$(dirname $0)/config/index.conf"
+PACKAGES_LIST="$(dirname $0)/config/packages.conf"
+
 SCRIPTS_PATH="$(dirname $0)/scripts"
 
 render_menu() {
+    declare -n local_list=$1
+
     render_banner
-    echo " DEV | OPS | INSTALLER & OTHER "
-    echo " ----------------------------- "
-    for key in "${!menu_items[@]}"; do
-        local desc="${menu_items[$key]%%:*}"
+
+    for key in "${!local_list[@]}"; do
+        local desc="${local_list[$key]%%:*}"
         echo " [$key] | $desc"
     done
-    echo -e ${YELLOW}    
+    echo -e "${message}${YELLOW}"
 }
 
 handle_choice() {
     local choice=$1
-    local script="${menu_items[$choice]#*:}"
+    declare -n local_list=$2
+
+    local value="${local_list[$choice]#*:}"
+    local current_page="${local_list[0]#*:}"
     
-    if [[ $script == "exit" ]]; then
-        exit
-    elif [[ -n $script && -f "$SCRIPTS_PATH/$script" ]]; then
-        . "$SCRIPTS_PATH/$script"
-    else
-        echo "Invalid choice or script not found. Press Enter to continue." && read -s
-    fi
+    case "$current_page" in
+        "index.conf")
+            if [[ $value == "exit" ]]; then
+                exit
+            elif [[ $value == "packages.conf" ]]; then
+                return 1
+            fi
+        ;;
+        "packages.conf")
+            if [[ $value == "exit" ]]; then
+                return 0
+            fi
+        ;;
+    esac
+    return -1
 }
 
-main() {
-    declare -A menu_items
 
-    load_config "$CONFIG_FILE" menu_items
+main() {
+    declare -A index_list
+    declare -A packages_list
+    load_config $INDEX_LIST index_list
+    load_config $PACKAGES_LIST packages_list
+
+    current_menu="index_list"
+    render_menu $current_menu
 
     while true; do
-        render_menu
         read -p " Enter your choice: " choice
-        echo
-        handle_choice "$choice"
+        
+        handle_choice "$choice" $current_menu        
+        case $? in
+            0 | 255)
+                current_menu="index_list"
+                ;;
+            1)
+                current_menu="packages_list"
+                ;;
+        esac
+        
+        render_menu $current_menu
     done
 }
 
