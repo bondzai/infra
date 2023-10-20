0d1818afc6d032d4937668b235ec55412d20657d
JamesBond <introbond.dev@gmail.com>
2023-10-20 13:35:41 +0700

    Add chrome &  brave

--- scripts/setup/setup_packages.sh
+++ scripts/setup/setup_packages.sh
@@ -10,3 +10,13 @@ setup_k8s() {
 setup_wireguard() {
     echo -e "setup wireguard..."
 }
+
+setup_chrome() {
+    sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+}
+
+setup_brave() {
+    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+}
