e71888109188b8d38eb8ab83639b8ced44e79867
JamesBond <introbond.dev@gmail.com>
2023-10-21 10:04:20 +0700

    Initial commit

---
+++ config/constants.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+declare -A REGEX_DICT
+
+# Store the awk logic for extracting key=value format from YAML in the dictionary
+REGEX_DICT[": "]="awk -F': ' '{if (NF==2) print \$1 \"=\" \$2}'"
