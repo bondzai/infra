4aef02a13c9ba5ad8cb87b0e3a3eee274a56749a
JamesBond <introbond.dev@gmail.com>
2023-10-21 19:53:11 +0700

    Add: Check network cli

--- scripts/system/cmds.sh
+++ scripts/system/cmds.sh
@@ -9,3 +9,21 @@ exec_cleanup() {
     sleep 1
     echo -e $GREEN"Trash empty!"
 }
+
+exec_check_network_status() {
+    local host="www.google.com"
+    local speedtest_command="speedtest-cli"
+
+    if ping -c 3 "$host" &> /dev/null; then
+        echo "Network status: Online"
+
+        if command -v "$speedtest_command" &> /dev/null; then
+            echo "Checking network speed..."
+            $speedtest_command
+        else
+            echo "Speedtest-cli is not installed. You can install it for speed testing."
+        fi
+    else
+        echo "Network status: Offline"
+    fi
+}
