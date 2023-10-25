21e7d138c60016c6df0629af21e98f220a9651d6
JamesBond <introbond.dev@gmail.com>
2023-10-25 20:56:35 +0700

    Cleanup

--- controllers/system/main.sh
+++ controllers/system/main.sh
@@ -4,11 +4,6 @@ exec_check_installed_packages() {
     dpkg --get-selections | grep -v deinstall | cut -f1
 }
 
-exec_update() {
-    sudo apt update && sudo apt upgrade -y
-    pprint -s "System is up to date!"
-}
-
 exec_cleanup() {
     trash-empty
     pprint -s "Trash empty!"
