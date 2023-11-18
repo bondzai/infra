49106d260dd306d0bdbfabd3fc99e47174bf32cc
JamesBond <introbond.dev@gmail.com>
2023-11-18 21:15:44 +0700

    feat: docker control

--- makefiles/go.mk
+++ makefiles/go.mk
@@ -84,3 +84,23 @@ gen-gitignore:
 test:
 	@echo "  >  Running tests...\n"
 	go test -v ./...
+
+## up: starts all containers in the background without forcing build
+up:
+	@echo "Starting Docker images..."
+	docker-compose up -d
+	@echo "Docker images started!"
+
+## up_build: stops docker-compose (if running), builds all projects and starts docker compose
+up_build: build_broker
+	@echo "Stopping docker images (if running...)"
+	docker-compose down
+	@echo "Building (when required) and starting docker images..."
+	docker-compose up --build -d
+	@echo "Docker images built and started!"
+
+## down: stop docker compose
+down:
+	@echo "Stopping docker compose..."
+	docker-compose down
+	@echo "Done!"
