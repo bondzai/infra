77ac17cc9fd3365581e64948251e6d118eca9bc3
JamesBond <introbond.dev@gmail.com>
2023-11-20 07:38:03 +0700

    tidy

--- controllers/backup/tools/git-clone/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source .env
-
-read -p "Enter the repository name: " repo_name
-
-clone_command="git clone https://$USERNAME:$PAT@github.com/swift-dynamics/$repo_name.git"
-
-$clone_command
